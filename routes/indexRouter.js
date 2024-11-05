d630611e5f74eacdcc9046e47e59ee5c3e2fcdff
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2024-11-05 14:08:01 -0500

    feat: refactor index page to show msg with no user

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const indexController = require('../controllers/indexController');
 const {validateUser} = require('../controllers/indexController');
+const { isAuth } = require("../helper/authMiddleware");
 const passport = require("passport");
 const indexRouter = Router();
 
 indexRouter.get('/', indexController.getIndexPage)
 indexRouter.get('/login', indexController.getLogin)
-indexRouter.get('/change-role', indexController.getBecomeMember)
+indexRouter.get('/change-role', isAuth, indexController.getBecomeMember)
 indexRouter.get('/logout', indexController.getLogout)
 
 
