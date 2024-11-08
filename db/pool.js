e394183def54b6e7672ce34b8b46446270ddb88c
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2024-11-08 14:54:17 -0500

    fix: change db string to connect to railway

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool = new Pool({
-    host: process.env.HOST, 
-    user: process.env.USER,
-    database: process.env.DB,
-    password: process.env.PASSWORD,
-    port: process.env.POOL_PORT 
+// const pool = new Pool({
+//     host: process.env.HOST, 
+//     user: process.env.USER,
+//     database: process.env.DB,
+//     password: process.env.PASSWORD,
+//     port: process.env.POOL_PORT 
+// });
+
+module.exports = new Pool({
+    connectionString: process.env.REMOTE_CONNECTION_STRING,
 });
 
 module.exports = pool;
